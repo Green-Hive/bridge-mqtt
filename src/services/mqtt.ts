1c7ed8eb1e9f469bdb55610ef9d1e169d2d6cb4a
plegulluche <plegulluche@gmail.com>
2024-05-31 11:31:21 +0200

    [ADD] adding dotenv to deps, [ADD] changing fetch method , fixing errors when calling the api route, [TODO] unit test.

--- src/services/mqtt.ts
+++ src/services/mqtt.ts
@@ -1,23 +1,31 @@
 import mqtt from 'mqtt';
-import axios, { AxiosError } from 'axios';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
 
 export const setupMQTT = () => {
+  // Se connecter au broker MQTT
   const mqttClient = mqtt.connect('mqtt://82.66.182.144:1883');
 
+  // Gérer l'événement de connexion au broker MQTT
   mqttClient.on('connect', () => {
     console.log('Connected to MQTT broker');
-    mqttClient.subscribe('test/sensor/data');
+    // S'abonner au topic 'sensor/data' pour recevoir les données des capteurs
+    mqttClient.subscribe('sensor/data');
   });
 
+  // Gérer l'événement de réception d'un message MQTT
   mqttClient.on('message', async (topic, message) => {
-    if (topic === 'test/sensor/data') {
+    // Vérifier si le message provient du topic 'sensor/data'
+    if (topic === 'sensor/data') {
+      // Parser les données JSON reçues
       const data = JSON.parse(message.toString());
-      console.log('Data received from MQTT:', data);
 
       // Créer l'objet de données à envoyer à l'application
       const hiveData = {
         hiveId: data.hive_id,
-        time: Date.now(),
+        time: data.data.timestamp,
         tempBottomLeft: data.data.tempBottomLeft,
         tempTopRight: data.data.tempTopRight,
         tempOutside: data.data.tempOutside,
@@ -30,18 +38,29 @@ export const setupMQTT = () => {
         magnetic_y: data.data.magnetic_y,
         magnetic_z: data.data.magnetic_z,
       };
+      console.log('hiveData:', hiveData);
 
       try {
-        // Envoyer les données à l'application via une requête POST
-        const response = await axios.post(`${process.env.APP_URL}/api/hives/data`, hiveData);
-        console.log('Data sent to application:', response.data);
-      } catch (error) {
-        if (axios.isAxiosError(error)) {
-          const axiosError = error as AxiosError;
-          console.error('Error sending data to application:', axiosError.message);
+        // Envoyer les données à l'application via une requête POST en utilisant fetch
+        const response = await fetch(`${process.env.APP_URL}/hives/data`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(hiveData),
+        });
+
+        // Vérifier si la réponse est OK (code de statut 200)
+        if (response.ok) {
+          const data = await response.json();
+          console.log('Data sent to application:', data);
         } else {
-          console.error('Unknown error:', error);
+          console.error('Error sending data to application:');
+          console.error('Status code:', response.status);
+          console.error('Response data:', await response.text());
         }
+      } catch (error) {
+        console.error('Error sending data to application:', error);
       }
     }
   });
